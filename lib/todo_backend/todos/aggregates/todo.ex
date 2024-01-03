a0f7fbe502254edfe451665c4eab66b342a6d230
Marco Torre <marco.torre@devlane.com>
2024-01-03 18:35:42 -0300

    added command and events to update todo

--- lib/todo_backend/todos/aggregates/todo.ex
+++ lib/todo_backend/todos/aggregates/todo.ex
@@ -7,10 +7,16 @@ defmodule TodoBackend.Todos.Aggregates.Todo do
   ]
 
   alias TodoBackend.Todos.Aggregates.Todo
-  alias TodoBackend.Todos.Events.TodoCreated
   alias TodoBackend.Todos.Commands.CreateTodo
-  alias TodoBackend.Todos.Events.TodoDeleted
   alias TodoBackend.Todos.Commands.DeleteTodo
+  alias TodoBackend.Todos.Commands.UpdateTodo
+
+  alias TodoBackend.Todos.Events.TodoCreated
+  alias TodoBackend.Todos.Events.TodoDeleted
+  alias TodoBackend.Todos.Events.TodoOrderUpdated
+  alias TodoBackend.Todos.Events.TodoTitleUpdated
+  alias TodoBackend.Todos.Events.TodoUncompleted
+  alias TodoBackend.Todos.Events.TodoCompleted
 
   def execute(%Todo{uuid: nil}, %CreateTodo{} = create) do
     %TodoCreated{
@@ -25,6 +31,27 @@ defmodule TodoBackend.Todos.Aggregates.Todo do
     %TodoDeleted{uuid: uuid}
   end
 
+  def execute(%Todo{} = todo, %UpdateTodo{} = update) do
+    completion_command =
+        if todo.completed != update.completed and not is_nil(update.completed) do
+            if update.completed do
+                %TodoCompleted{uuid: todo.uuid}
+            else
+                %TodoUncompleted{uuid: todo.uuid}
+            end
+        end
+
+    title_command =
+        if todo.title != update.title and not is_nil(update.title),
+            do: %TodoTitleUpdated{uuid: todo.uuid, title: update.title}
+
+    order_command =
+        if todo.order != update.order and not is_nil(update.order),
+            do: %TodoOrderUpdated{uuid: todo.uuid, order: update.order}
+
+    [completion_command, title_command, order_command] |> Enum.filter(&Function.identity/1)
+  end
+
   def apply(%Todo{} = todo, %TodoCreated{} = created) do
       %Todo{
           todo
@@ -37,5 +64,21 @@ defmodule TodoBackend.Todos.Aggregates.Todo do
 
   def apply(%Todo{uuid: uuid}, %TodoDeleted{uuid: uuid}) do
     nil
+  end
+
+  def apply(%Todo{} = todo, %TodoCompleted{}) do
+    %Todo{todo | completed: true}
+  end
+
+  def apply(%Todo{} = todo, %TodoUncompleted{}) do
+    %Todo{todo | completed: false}
+  end
+
+  def apply(%Todo{} = todo, %TodoTitleUpdated{title: title}) do
+    %Todo{todo | title: title}
+  end
+
+  def apply(%Todo{} = todo, %TodoOrderUpdated{order: order}) do
+    %Todo{todo | order: order}
   end
 end
